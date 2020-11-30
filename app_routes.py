e935cb56a67425e47f3749e65837144b0c4ba85f
spennington <spennington13@gmail.com>
2020-11-30 17:35:07 -0500

    routes
    do not have last two done. little confused.

--- app_routes.py
+++ app_routes.py
@@ -35,14 +35,41 @@ def home_page ():
 @app.route("/api/v1.0/precipitation")
 def precip():
     previous = dt.date(2017,8,23) - dt.timedelta(days = 365)
-    precip = session.query(measurement.date, measurement.prcp).filter(measurement.date >= previous).all()
+    precip = session.query(measurement.date, measurement.prcp).\
+    filter(measurement.date >= previous).all()
+    
+    precipitation = []
+    for i in precip:
+        dict = {"Date":precip[0], "Precipitation":precip[1]}
+        precipitation.append(dict)
+    return jsonify(precipitation)
     
     
 @app.route("/api/v1.0/stations")
 def stations ():
     first1 = session.query(station.name, station.station).all()
     
+    stations = []
+    for i in first1:
+        dict = {"Name": first1[0], "Station Name":first1[1]}
+        stations.append(dict)
     
 @app.route("/api/v1.0/tobs")
 def tobs():
+    previous_temp = dt.date(2017,8,23) - dt.timedelta(days = 365)
+    temp = session.query(measurement.date,measurement.tobs).\
+    filter(measurement.station =='USC00519281',measurement.date>=previous_temp).all()
+    
+    temps = []
+    for e in temps:
+        dict = {"Date":temp[0], "Temperature":temp[1]}
+        temps.append(dict)
+        
+@app.route("/api/v1.0/<start>")
+
+
+
+@app.route("/api/v1.0/<start>/<end>")
+    
+    
     
